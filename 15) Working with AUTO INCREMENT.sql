8b446efdf531d3641a4225621f0008ee81174580
Arjun <varjun609@gmail.com>
2024-02-10 20:42:02 +0530

    Working with AUTO INCREMENT

---
+++ 15) Working with AUTO INCREMENT.sql
@@ -0,0 +1,11 @@
+CREATE TABLE unique_cats5 (
+    cat_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+
+-- INSERT INTO unique_cats5(name,age)
+-- VALUES ('Alex',10);
+
+-- SELECT * FROM unique_cats5;
