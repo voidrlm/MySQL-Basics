8101ccc712abad7f43b1f7b400bb9ceb072caaed
Arjun <varjun609@gmail.com>
2024-02-10 20:02:23 +0530

    Creating tables with default values

---
+++ 13) Creating tables with default values.sql
@@ -0,0 +1,7 @@
+CREATE TABLE phone(brand VARCHAR(50) NOT NULL,model VARCHAR(150) NOT NULL,userName VARCHAR(150) default 'unknown');
+
+
+-- INSERT INTO phone(brand,model,userName)
+-- VALUES ('apple','14 plus','joe');
+
+-- SELECT * FROM PHONE;
