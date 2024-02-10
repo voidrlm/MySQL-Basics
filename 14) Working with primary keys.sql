f56338d040fce27903650aa815f29d1d7da592c2
Arjun <varjun609@gmail.com>
2024-02-10 20:21:18 +0530

    Working with primary keys

---
+++ 14) Working with primary keys.sql
@@ -0,0 +1,11 @@
+CREATE TABLE unique_cats (
+    cat_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+
+-- INSERT INTO unique_cats(cat_id,name,age)
+-- VALUES (123,'Alex',10);
+
+-- SELECT * FROM unique_cats;
